7b22817caf18b936d1097fcfc2e577bf300866ca
Bharath Honakatti <bharathonakatti2652005@gmail.com>
2024-12-15 00:38:53 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Event-Calendar-Application/",
   plugins: [react()],
+  base:"/Event-Calendar-Application/",
 })
